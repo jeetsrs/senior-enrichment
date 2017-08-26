afbd14310d11d2f2d3ea931f58c3cafa66e6eba0
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-08-26 12:50:45 -0400

    basic sequelize model built

--- db/models/campus.js
+++ db/models/campus.js
@@ -3,12 +3,8 @@ var Sequelize = require('sequelize')
 var db = require('../index.js')
 
 
-module.exports = db.define('user', {
+module.exports = db.define('campus', {
   name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  cohort: {
     type: Sequelize.STRING,
     allowNull: false
   }
