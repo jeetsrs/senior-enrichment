7fbb54ac2aa0154c62900d6f4f76380fc76d1120
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-08-27 21:30:24 -0400

    some more work on React... but its not working

--- OLD_app/main.jsx
+++ OLD_app/main.jsx
@@ -4,10 +4,12 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
+// import Root from './components/Root';
 import View from './components/View';
 
 render (
   <Provider store={store}>
+    {/* <Root/> */}
     <View campusList={store.campus}/>
   </Provider>,
   document.getElementById('main')
--- app/store.jsx
+++ app/store.jsx
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
 import createLogger from 'redux-logger'; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from 'redux-thunk'; // https://github.com/gaearon/redux-thunk
 import axios from 'axios';
 
+const initialState = {
+  students: [],
+  campuses: [],
+  newStudent: ''
+};
+
 // ACTION TYPES
 const GET_STUDENTS = 'GET_STUDENTS';
 const GET_CAMPUSES = 'GET_CAMPUSES';
+const ADD_CAMPUS = 'ADD_CAMPUS';
 const ADD_STUDENT = 'ADD_STUDENT';
 
 // ACTION CREATORS
@@ -18,23 +24,26 @@ export function getCampuses (campuses) {
   const action = { type: GET_CAMPUSES, campuses };
   return action;
 };
+export function addCampus (campus) {
+  const action = { type: ADD_CAMPUS, campus };
+  return action;
+};
 export function addStudent (student) {
   const action = { type: ADD_STUDENT, student };
   return action;
 };
 
-
 // THUNK CREATORS
 export function fetchStudents () {
-    return function thunk (dispatch) {
-      return axios.get('/api/student')
-        .then(res => res.data)
-        .then(students => {
-          const action = getStudents(students);
-          dispatch(action);
-        });
-    };
-}
+  return function thunk (dispatch) {
+    return axios.get('/api/student')
+      .then(res => res.data)
+      .then(students => {
+        const action = getStudents(students);
+        dispatch(action);
+      });
+  };
+};
 export function fetchCampuses () {
   return function thunk (dispatch) {
     return axios.get('/api/campus')
@@ -44,6 +53,25 @@ export function fetchCampuses () {
         dispatch(action);
       });
   };
-}
+};
+
+// REDUCER
+const rootReducer = function(state = initialState, action) {
+  switch(action.type) {
+    case GET_STUDENTS:
+      // I think I am assigning the students object here into the state - NOT SURE :(
+      return Object.assign({}, state, { students: action.students });
+    case GET_CAMPUSES:
+      // I think I am assigning the campus object here into the state - NOT SURE :(
+      return Object.assign({}, state, { campuses: action.campuses });
+    case ADD_CAMPUS:
+      // I think I am adding a campus to the list here - NOT SURE WHAT TO DO HERE :(
+      return Object.assign({}, state, {campuses: state.campuses.concat(action.campus)});
+    case ADD_STUDENT:
+      // I think I am adding a student to the list here - NOT SURE WHAT TO DO HERE :(
+      return Object.assign({}, state, {students: state.students.concat(action.student)});
+    default: return state;
+  }
+};
 
 export default createStore(rootReducer, applyMiddleware(thunkMiddleware, createLogger()));
